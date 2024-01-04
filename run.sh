298594841cee529686ce57564e405af5d026b393
Your Name <you@example.com>
2024-01-04 02:08:58 +0000

    ..

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 export JAX_LOG_COMPILES=1
-python3 src/train_dpr.py --output_dir blabla --train_dir https://huggingface.co/datasets/iohadrubin/nq/resolve/main/data/train-00000-of-00012-aebee16ac9d5ed6f.parquet
+python3 src/train_dpr.py --output_dir blabla --dataset_name iohadrubin/nq
+# --train_dir https://huggingface.co/datasets/iohadrubin/nq/resolve/main/data/train-00000-of-00012-aebee16ac9d5ed6f.parquet
