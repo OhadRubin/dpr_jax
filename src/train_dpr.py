b9927f9f83dbde6b6ce8d2ed5864af2b8392ad1c
Your Name <you@example.com>
2024-01-05 02:10:56 +0000

    ..

--- src/train_dpr.py
+++ src/train_dpr.py
@@ -679,7 +679,7 @@ def main():
 
 
     logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {len(train_dataset)}")
+    # logger.info(f"  Num examples = {len(train_dataset)}")
     logger.info(f"  Instantaneous batch size per device = {training_args.per_device_train_batch_size}")
     logger.info(f"  Total train batch size (w. parallel & distributed) = {train_batch_size}")
     logger.info(f"  Total optimization steps = {num_train_steps}")
