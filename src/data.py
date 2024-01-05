788bbb6339c7b53e3c4968d18cbbed4a7d61559b
Your Name <you@example.com>
2024-01-05 11:27:50 +0000

    ..

--- src/data.py
+++ src/data.py
@@ -29,6 +29,76 @@ def extract_dpr_examples(element, tokenizer):
             # del examples_dict[chunk_id]
     return final_list
 
+        
+# from functools import partial
+# from multiprocessing import Process, Queue
+# from functools import partial
+
+# # Define the worker function template
+# def worker(input_queue, output_queue, process_function, limit=None):
+#     cnt=0
+
+#     while True:
+#         item = input_queue.get()
+#         if item is None:
+#             break
+        
+#         for result in process_function(item):
+#             output_queue.put(result)
+#         if limit is not None:
+#             if cnt>limit:
+#                 break
+#         cnt+=1
+#     output_queue.put(None)  # Signal the next worker to shut down
+
+
+
+
+
+
+# def data_generator(method, dataset, pooling_obj, lm_tokenizer,
+#                    retriever_tokenizer, K, chunk_length, max_neighbors,
+#                    batch_size,
+#                    limit_books=None
+#                    ):
+#     process_object = partial(process_book,method=method, pooling_obj=pooling_obj,
+#                              lm_tokenizer=lm_tokenizer, retriever_tokenizer=retriever_tokenizer,K=K)
+#     process_example = partial(flatten_element,chunk_length=chunk_length,max_neighbors=max_neighbors)
+#     # process_stack = partial(stack_candidates,batch_size=batch_size)
+#     num_workers = 2  # Two stages in your pipeline
+
+#     # Create the queues for communication
+#     queues = [Queue() for _ in range(num_workers + 1)]
+#     output_queue = queues[-1]
+
+#     # Create and start the workers for each stage
+#     book_worker = Process(target=worker, args=(queues[0], queues[1], process_object), kwargs=dict(limit=limit_books))
+#     flatten_worker = Process(target=worker, args=(queues[1], queues[2], process_example))
+#     # stacking_worker = Process(target=worker, args=(queues[2], queues[3], process_stack))
+#     book_worker.start()
+#     flatten_worker.start()
+#     try:
+#         # Feed the dataset items to the first queue
+#         for obj in enumerate(dataset):
+#             queues[0].put(obj)
+
+#         # Signal the first worker to shut down after all items are queued
+#         queues[0].put(None)
+#         def my_iter():
+#             while True:
+#                 result = output_queue.get()
+#                 if result is None:
+#                     break
+#                 yield result
+#         for batch in stack_candidates(my_iter(), batch_size):
+#             yield batch
+#         # Collect the final results
+        
+#     finally:
+#         # Wait for all workers to finish
+#         book_worker.join()
+#         flatten_worker.join()
+        
 def get_dataset(name:str, split:str):
     shard_id = jax.process_index()
     num_shards=jax.process_count()
