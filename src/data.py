0e1fa2eb65ebe98b341e7ec29f27ef69ab4bcec5
Your Name <you@example.com>
2024-01-07 02:31:56 +0000

    ..

--- src/data.py
+++ src/data.py
@@ -240,7 +240,7 @@ def get_dataloader(split, batch_size, model_args, data_args):
     time.sleep(10)
     print("waking up")
     iterable = IterableDatasetWrapper(data_stream,split=split) 
-    dloader= DataLoader(iterable,
+    dloader= DataLoader(itertools.cycle(iterable),
                             batch_size=batch_size,
                             collate_fn=lambda v: package(v)
                             )
