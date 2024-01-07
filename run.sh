69b0d0762772388903b92c2cbe4fe08026f5aa61
Your Name <you@example.com>
2024-01-07 00:40:09 +0000

    ..

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 
 
-export BUCKET=meliad_eu2
-export LOGURU_LEVEL=INFO
-export JAX_LOG_COMPILES=1
-# /usr/bin/env python3 src/data.py test_stuff
+# export BUCKET=meliad_eu2
+# export LOGURU_LEVEL=INFO
+# export JAX_LOG_COMPILES=1
+# # /usr/bin/env python3 src/data.py test_stuff
 
-export WANDB_NAME="wd-1e-1_b2-0.95_lr6e-5_bs128_v16"
-/usr/bin/env python3 src/train_dpr.py --output_dir blabla2  \
-    --adam_beta2 0.95 \
-    --weight_decay 1e-1 \
-    --learning_rate 1e-5 \
-    --per_device_train_batch_size 128 \
-     --dataset_name iohadrubin/nq
+# export WANDB_NAME="wd-1e-1_b2-0.95_lr6e-5_bs128_v16"
+# /usr/bin/env python3 src/train_dpr.py --output_dir blabla2  \
+#     --adam_beta2 0.95 \
+#     --weight_decay 1e-1 \
+#     --learning_rate 1e-5 \
+#     --per_device_train_batch_size 128 \
+#      --dataset_name iohadrubin/nq
